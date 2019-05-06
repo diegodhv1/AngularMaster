8f69504b5f5861b2f6d3301ea41cc99f33d6202b
diego <diegodhv1@gmail.com>
2019-05-05 23:03:48 -0500

    adding admin pro

---
+++ adminpro/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NopagesfoundComponent } from './shared/nopagesfound/nopagesfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NopagesfoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
