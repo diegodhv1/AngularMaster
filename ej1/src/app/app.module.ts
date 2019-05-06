2429e58eaa2940c114b5e0d785ae48217670dc63
diego <diegodhv1@gmail.com>
2019-05-05 21:18:59 -0500

    avances

--- ej1/src/app/app.module.ts
+++ ej1/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MainpageComponent } from './pages/mainpage.component';
 import { AboutComponent } from './pages/about.component';
-
+import { Home2Component } from './pages/home/home2.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainpageComponent,
-    AboutComponent
+    AboutComponent,
+    Home2Component
   ],
   imports: [
     BrowserModule
---
+++ ej1/src/app/pages/home/home2.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home2',
+  template: `
+    <p>
+      home2 works!
+    </p>
+  `
+})
+export class Home2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
